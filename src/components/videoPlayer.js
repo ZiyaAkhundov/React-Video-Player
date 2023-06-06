3ee0f7110ac5716e0769ae14d830a17addf025b8
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-06-06 22:23:22 +0400

    add mute

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -14,6 +14,7 @@ function VideoPlayer(){
     const [miniPlayer, setminiPlayer] = useState(false);
     const videoContainerRef = useRef(null);
     const videoRef = useRef(null);
+    const volumeSliderRef= useRef(null);
 
     const toggleFullScreenMode=()=>{
         if (!document.fullscreenElement) {
@@ -52,7 +53,40 @@ function VideoPlayer(){
   const toggleTheaterMode=()=> {
     settheaterMode((prevTheaterMode) => !prevTheaterMode);
 }
+const volumeInput=(e)=> {
+    const newVolume = e.target.value;
+    videoRef.current.volume = newVolume;
+    videoRef.current.muted = newVolume === "0";
+    setVolume(newVolume);
+    // volumeSliderRef.current.value = volume
+}
+const toggleMute = ()=> {
+    // setVolume(0)
+    videoRef.current.muted = !videoRef.current.muted;
+    console.log(volumeSliderRef.current.value)
+    if(videoRef.current.muted){
+        setVolume(0)
+    }
+  }
+ 
     useEffect(() => {
+        videoRef.current.addEventListener("volumechange", () => {
+            volumeSliderRef.current.value = videoRef.current.volume
+            let volumeLevel
+            if (videoRef.current.muted || volume === 0) {
+                volumeSliderRef.current.value = 0;
+              volumeLevel = "muted"
+            } else if (videoRef.current.volume >= 0.5) {
+              volumeLevel = "high"
+              setVolume(videoRef.current.volume)
+            }else if(videoRef.current.volume===0){
+                volumeLevel = "muted"
+            }
+             else {
+              volumeLevel = "low"
+            }
+            videoContainerRef.current.dataset.volumeLevel = volumeLevel
+          })
         const handleKeyDown = (e) => {
           const tagName = e.target.tagName.toLowerCase();
           const key = e.key.toLowerCase();
@@ -74,7 +108,7 @@ function VideoPlayer(){
               toggleMiniPlayerMode();
               break;
             case 'm':
-              // toggleMute();
+              toggleMute();
               break;
             case 'arrowleft':
             case 'j':
@@ -116,7 +150,7 @@ function VideoPlayer(){
             </svg>
           </button>
           <div className="volume-container">
-            <button className="mute-btn">
+            <button className="mute-btn" onClick={toggleMute}>
               <svg className="volume-high-icon" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z" />
               </svg>
@@ -127,7 +161,7 @@ function VideoPlayer(){
                 <path fill="currentColor" d="M12,4L9.91,6.09L12,8.18M4.27,3L3,4.27L7.73,9H3V15H7L12,20V13.27L16.25,17.53C15.58,18.04 14.83,18.46 14,18.7V20.77C15.38,20.45 16.63,19.82 17.68,18.96L19.73,21L21,19.73L12,10.73M19,12C19,12.94 18.8,13.82 18.46,14.64L19.97,16.15C20.62,14.91 21,13.5 21,12C21,7.72 18,4.14 14,3.23V5.29C16.89,6.15 19,8.83 19,12M16.5,12C16.5,10.23 15.5,8.71 14,7.97V10.18L16.45,12.63C16.5,12.43 16.5,12.21 16.5,12Z" />
               </svg>
             </button>
-            <input className="volume-slider" type="range" min="0" max="1" step="any" />
+            <input ref={volumeSliderRef} onChange={volumeInput} className="volume-slider" type="range" min="0" max="1" step="any" />
           </div>
           <div className="duration-container">
             <div className="current-time">0:00</div>
